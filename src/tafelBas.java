08fa03104487b9d02f215d4bfefc898d1b1679e2
BKranenbarg <tenazb@gmail.com>
2020-09-24 11:04:15 +0200

    Version 3 met code tafel van 93

--- src/tafelBas.java
+++ src/tafelBas.java
@@ -1,12 +1,13 @@
 public class tafelBas {
     public static void main(String[] args) {
         //string voor weegeven tafels
+        String output;
         //variable getal tafel
-        int y = 8;
+        int y = 93;
 
         // getallen van een tot tien in lus aanmaken
         for (int x=1; x<=10; x++) {
-            String output = x + " x " + y + " = ";
+            output = x + " x " + y + " = ";
                 int z = x * y;
                 output = output + z;
                 System.out.println(output);
