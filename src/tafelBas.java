cc2a1f172e562da40ba3caedb86542326317165a
BKranenbarg <tenazb@gmail.com>
2020-09-24 10:34:09 +0200

    Version 2 met code tafel van 8

--- src/tafelBas.java
+++ src/tafelBas.java
@@ -1,2 +1,15 @@
 public class tafelBas {
+    public static void main(String[] args) {
+        //string voor weegeven tafels
+        //variable getal tafel
+        int y = 8;
+
+        // getallen van een tot tien in lus aanmaken
+        for (int x=1; x<=10; x++) {
+            String output = x + " x " + y + " = ";
+                int z = x * y;
+                output = output + z;
+                System.out.println(output);
+       }
+    }
 }
